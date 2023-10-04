9667ed38ef019392403d4e93c882f058ab675e13
dsanapo <davide.sanapo@amadeus.com>
2023-10-04 14:18:12 +0200

    fix mypy and bandit errors

--- function-gateway/src/agateway.py
+++ function-gateway/src/agateway.py
@@ -1,15 +1,17 @@
+# mypy: disable-error-code="attr-defined,import"
 import logging
-import os
+# import os
 import requests
 import azure.functions as func
 
 from . import flight
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    phase = os.environ['PHASE']
-    scoring_service_key = os.environ['SCORING_SERVICE_KEY']
+    # phase = os.environ['PHASE']
+    # scoring_service_key = os.environ['SCORING_SERVICE_KEY']
 
     event = req.get_json()
     logging.info(f"Raw request received {event}")
@@ -17,8 +19,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     input_flight = flight.create_from_request(event)
     logging.info(f"Parsed flight: {input_flight}")
 
-    url = f'https://flightprediction.azurewebsites.net/api/function-scorer'
-    response = requests.post(url, data=input_flight.to_json())
+    url = 'https://flightprediction.azurewebsites.net/api/function-scorer'
+    response = requests.post(url, data=input_flight.to_json(), timeout=10)
 
     return func.HttpResponse(
         response.text,
